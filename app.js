ff04bd8622a40a8d7f35912babf47d65b2ce404e
hilal dedek <hdedek03@gmail.com>
2023-07-28 20:00:06 +0300

    listed

--- app.js
+++ app.js
@@ -9,37 +9,56 @@ const result=document.getElementById("noteResult");
 const numberResult=document.getElementById("numberResult");
 const lessonResult=document.getElementById("lessonResult");
 const photoResult=document.getElementById("photoResult");
-
+const studentUl=document.getElementById("ul");
+const person=[];
+var personnote="";
+var counter=0;
 submit.addEventListener("click",function(){
-    fullnameResult.innerText=`${fullname.value}`
-    numberResult.innerText=`${number.value}`
-    lessonResult.innerText=`${lesson.value}`
-    
+    counter++;
+    // fullnameResult.innerText=`${fullname.value}`
+    // numberResult.innerText=`${number.value}`
+    // lessonResult.innerText=`${lesson.value}`
     
     if(note.value<=100 && note.value>=95){
-        result.innerText=`A`;
+        personnote="A";
     }else if(note.value<=94 && note.value>=90){
-        result.innerText=`A-`;
+        personnote="A-";
     }else if(note.value<=89 && note.value>=86){
-        result.innerText=`B+`;
+        personnote="B+";
     }else if(note.value<=85 && note.value>=82){
-        result.innerText=`B`;
+        personnote="B";
     }else if(note.value<=81 && note.value>=78){
-        result.innerText=`B-`;
+        personnote="B-";
     }else if(note.value<=77 && note.value>=74){
-        result.innerText=`C+`;
+        personnote="C+";
     }else if(note.value<=73 && note.value>=70){
-        result.innerText=`C`;
+        personnote="C";
     }else if(note.value<=69 && note.value>=63){
-        result.innerText=`C-`;
+        personnote="C-";
     }else if(note.value<=62 && note.value>=57){
-        result.innerText=`D+`;
+        personnote="D+";
     }else if(note.value<=56 && note.value>=51){
-        result.innerText=`D`;
+        personnote="D";
     }else if(note.value<=50 && note.value>=0){
-        result.innerText=`F`;
+        personnote="F";
     }
-    else{
-        result.innerText=`GEÇERSİZ NOT`;
-    }
+    // result.innerText=`${personnote}`;
+    person.push(fullname.value,number.value,lesson.value,note.value,personnote);
+    console.log(person);
+    localStorage.setItem(`data${counter}`,person);
+    function printLi(personnote){
+        console.log(personnote);
+        const li=document.createElement("li");
+        const p = document.createElement("p");
+        const pTextNode = document.createTextNode(person);
+        p.appendChild(pTextNode);
+        console.log(pTextNode);
+        li.appendChild(p);
+        studentUl.appendChild(li);
+        console.log(li);
+}
+    printLi();
+    for (var i = person.length; i > 0; i--) {
+        person.pop();
+       }
 })
---
+++ yedekapp.js
@@ -0,0 +1,45 @@
+// const fullname=document.getElementById("fullname");
+// const number=document.getElementById("number");
+// const lesson=document.getElementById("lesson");
+// const note=document.getElementById("note");
+// const photo=document.getElementById("photo");
+// const submit=document.getElementById("submit");
+// const fullnameResult=document.getElementById("fullnameResult");
+// const result=document.getElementById("noteResult");
+// const numberResult=document.getElementById("numberResult");
+// const lessonResult=document.getElementById("lessonResult");
+// const photoResult=document.getElementById("photoResult");
+
+// submit.addEventListener("click",function(){
+//     fullnameResult.innerText=`${fullname.value}`
+//     numberResult.innerText=`${number.value}`
+//     lessonResult.innerText=`${lesson.value}`
+    
+    
+//     if(note.value<=100 && note.value>=95){
+//         result.innerText=`A`;
+//     }else if(note.value<=94 && note.value>=90){
+//         result.innerText=`A-`;
+//     }else if(note.value<=89 && note.value>=86){
+//         result.innerText=`B+`;
+//     }else if(note.value<=85 && note.value>=82){
+//         result.innerText=`B`;
+//     }else if(note.value<=81 && note.value>=78){
+//         result.innerText=`B-`;
+//     }else if(note.value<=77 && note.value>=74){
+//         result.innerText=`C+`;
+//     }else if(note.value<=73 && note.value>=70){
+//         result.innerText=`C`;
+//     }else if(note.value<=69 && note.value>=63){
+//         result.innerText=`C-`;
+//     }else if(note.value<=62 && note.value>=57){
+//         result.innerText=`D+`;
+//     }else if(note.value<=56 && note.value>=51){
+//         result.innerText=`D`;
+//     }else if(note.value<=50 && note.value>=0){
+//         result.innerText=`F`;
+//     }
+//     else{
+//         result.innerText=`GEÇERSİZ NOT`;
+//     }
+// })
